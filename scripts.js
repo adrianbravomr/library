54ea1897367a26ecdbe35c89f957a7f38b64dbda
adrianbravomr <adrianbravomr@gmail.com>
2022-08-14 09:10:00 -0400

    Changed to classes in library/books objects

--- scripts.js
+++ scripts.js
@@ -1,22 +1,123 @@
-let library=[];
-
-function Book(title, author, pages, isRead, cover='', summary='', color=''){
-    this.title = title
-    this.author = author
-    this.summary = summary
-    this.pages = pages
-    this.isRead = isRead
-    this.cover = cover
-    this.color = color
+class Library{
+    constructor(){
+        this.books=[]
+    }
+    count(){
+        return this.books.length;
+    }
+    pushBook(book){
+        //console.log(`Adding '${book.title}' (${book.author}) to library`);
+        this.books.push(book);
+    }
+    showBook(book){
+        //console.log(book)
+        let index = library.count()-1;
+        let bookView=document.createElement("div");
+        bookView.classList.add("book");
+        if(book.color!="#83a5e8") bookView.style.backgroundColor=book.color;
+        bookView.dataset.index=index;
+        bookView.id=index;
+    
+        if (book.cover!=""){
+            bookView.classList.add("book-cover");
+            let cover=document.createElement("img");
+            cover.classList.add("book-cover");
+            cover.src=book.cover;
+    
+            bookView.append(cover);
+        }
+    
+        let title=bookData(book.title,'book-title');
+        title.classList.add('title-show');
+        if(book.summary!="") title.addEventListener('click',e=>{
+            summary.classList.add('summary-show');
+            title.classList.remove('title-show');
+        });
+    
+        let author=bookData(book.author,'book-author');
+        let summary=bookData(book.summary,'book-summary');
+    
+        summary.addEventListener('click',event=>{
+            title.classList.add('title-show');
+            summary.classList.remove('summary-show');
+        });
+    
+        let pages=bookData('');
+        if(book.pages!='') pages=bookData(book.pages,'book-pages');
+    
+        let isRead=document.createElement('button');
+        library.read(book,isRead);
+    
+        isRead.addEventListener('click',e=>{
+            let bookInst=library.books[bookView.dataset.index];
+            bookInst.toggleRead();
+            library.read(book,isRead);
+        });
+        //let isRead=bookData(book.isRead,'book-isRead');
+    
+        let rmv=document.createElement('button');
+        rmv.classList.add('remove-button');
+        rmv.innerText="Remove";
+    
+        rmv.addEventListener('click',e => {
+            bookView.remove();
+        });
+    
+        let bookCont=document.createElement("div");
+        bookCont.classList.add("book-container");
+        bookCont.append(title,author,summary,pages,isRead,rmv);
+    
+        bookView.append(bookCont);
+    
+        bookView.addEventListener('mouseenter',(e)=>{
+            isRead.classList.add('isRead-show');
+            rmv.classList.add('remove-show');
+            if(book.cover!="") bookView.firstChild.classList.add('cover-hover');
+        })
+        bookView.addEventListener('mouseleave',(e)=>{
+            isRead.classList.remove('isRead-show');
+            rmv.classList.remove('remove-show');
+            if(book.cover!="") bookView.firstChild.classList.remove('cover-hover');
+        })
+        libraryView.appendChild(bookView);
+    }
+    read(book,button){
+        button.classList.add('readButton');
+        if(book.isRead){
+            button.innerText='Finished';
+            button.classList.add('finished');
+            button.classList.remove('not-read');
+        }
+        else{
+            button.innerText='Not read';
+            button.classList.add('not-read');
+            button.classList.remove('finished');
+        }
+    }
+    showBooks(){
+        //console.log('Books in library:');
+        library.books.forEach((book,index) => {
+            this.showBook(book);
+        })
+    }
 }
 
-Book.prototype = {    
-    toggleRead : function(){
-        this.isRead = !this.isRead;
-        return this.isRead;
-    },
-    info : function () {
-        info = this.title+" by "+this.author+", "+this.pages+" pages, "
+class Book{
+    constructor(title, author, pages, isRead, cover='', summary='', color=''){
+        this.title = title
+        this.author = author
+        this.summary = summary
+        this.pages = pages
+        this.isRead = isRead
+        this.cover = cover
+        this.color = color
+    }
+    toggleRead(){
+        this.isRead=!this.isRead;
+        return this.isRead; 
+    }
+    info(){
+        let info = this.title+" by "+this.author+", "+this.pages+" pages, "
         if(this.isRead){
             return info+" finished reading"
         }
@@ -24,13 +125,6 @@ Book.prototype = {
             return info+" not read yet"
         }
     }
-}
-
-
-function addBook(book){
-    //console.log(`Adding '${book.title}' (${book.author}) to library`);
-    library.push(book);
-    return;
 }
 
 function formInput(type,placeholder='',required=false){
@@ -56,9 +150,9 @@ function formLabel(name,innerText=''){
 
 function submitForm(form){
     const newBook = new Book(form.Title.value,form.Author.value,form.Pages.value, form.isRead.checked,form.Cover.value,form.Summary.value,form.Color.value);
-    addBook(newBook);
     document.querySelector('.addBook-container').remove();
-    appendBook(newBook,library.length-1);
+    library.pushBook(newBook);
+    library.showBook(newBook);
 }
 
 
@@ -122,115 +216,19 @@ function addBookForm(){
 
 function bookData(data,classes=''){
     let b=document.createElement("div");
-    if(classes!='')b.classList.add(classes);
+    if(classes!='') b.classList.add(classes);
     b.innerText=data;
     return b;
 }
 
-function readButton(book,button){
-    button.classList.add('readButton');
-    if(book.isRead){
-        button.innerText='Finished';
-        button.classList.add('finished');
-        button.classList.remove('not-read');
-    }
-    else{
-        button.innerText='Not read';
-        button.classList.add('not-read');
-        button.classList.remove('finished');
-    }
-}
+let library = new Library();
 
-function appendBook(book,index){
-    //console.log(book)
-    let bookView=document.createElement("div");
-    bookView.classList.add("book");
-    if(book.color!="#83a5e8") bookView.style.backgroundColor=book.color;
-    bookView.dataset.index=index;
-    bookView.id=index;
+const libraryView = document.querySelector(".library");
 
-    if (book.cover!=""){
-        bookView.classList.add("book-cover");
-        let cover=document.createElement("img");
-        cover.classList.add("book-cover");
-        cover.src=book.cover;
+document.querySelector("#newbook").addEventListener("click",addBookForm);
 
-        bookView.append(cover);
-    }
+library.pushBook(new Book('The Hobbit','J.R.R. Tolkien',295, false));
 
-    let title=bookData(book.title,'book-title');
-    title.classList.add('title-show');
-    if(book.summary!="") title.addEventListener('click',e=>{
-        summary.classList.add('summary-show');
-        title.classList.remove('title-show');
-    });
+library.pushBook(new Book('A Game of Thrones','J.R.R. Martin',694,false,'https://m.media-amazon.com/images/I/51NF6gOqnuL.jpg','In the mythical continent of Westeros, several powerful families fight for control of the Seven Kingdoms. As conflict erupts in the kingdoms of men, an ancient enemy rises once again to threaten them all. Meanwhile, the last heirs of a recently usurped dynasty plot to take back their homeland from across the Narrow Sea.'));
 
-    let author=bookData(book.author,'book-author');
-    let summary=bookData(book.summary,'book-summary');
-
-    summary.addEventListener('click',event=>{
-        title.classList.add('title-show');
-        summary.classList.remove('summary-show');
-    });
-
-    let pages=bookData('');
-    if(book.pages!='') pages=bookData(book.pages,'book-pages');
-
-    let isRead=document.createElement('button');
-    readButton(book,isRead);
-
-    isRead.addEventListener('click',e=>{
-        let bookInst=library[bookView.dataset.index];
-        bookInst.toggleRead();
-        readButton(book,isRead);
-    });
-    //let isRead=bookData(book.isRead,'book-isRead');
-
-    let rmv=document.createElement('button');
-    rmv.classList.add('remove-button');
-    rmv.innerText="Remove";
-
-    rmv.addEventListener('click',e => {
-        bookView.remove();
-    });
-
-    let bookCont=document.createElement("div");
-    bookCont.classList.add("book-container");
-    bookCont.append(title,author,summary,pages,isRead,rmv);
-
-    bookView.append(bookCont);
-
-    bookView.addEventListener('mouseenter',(e)=>{
-        isRead.classList.add('isRead-show');
-        rmv.classList.add('remove-show');
-        if(book.cover!="") bookView.firstChild.classList.add('cover-hover');
-    })
-    bookView.addEventListener('mouseleave',(e)=>{
-        isRead.classList.remove('isRead-show');
-        rmv.classList.remove('remove-show');
-        if(book.cover!="") bookView.firstChild.classList.remove('cover-hover');
-    })
-    libraryList.appendChild(bookView);
-}
-
-function showBooks(){
-    //console.log('Books in library:');
-    library.forEach((book,index) => {
-        appendBook(book,index);
-    })
-}
-
-const libraryList = document.querySelector(".library");
-
-const newBookButton = document.querySelector("#newbook");
-newBookButton.addEventListener("click",addBookForm);
-
-let book;
-
-book = new Book('The Hobbit','J.R.R. Tolkien',295, false)
-addBook(book);
-
-book = new Book('A Game of Thrones','J.R.R. Martin',694,false,'https://m.media-amazon.com/images/I/51NF6gOqnuL.jpg','In the mythical continent of Westeros, several powerful families fight for control of the Seven Kingdoms. As conflict erupts in the kingdoms of men, an ancient enemy rises once again to threaten them all. Meanwhile, the last heirs of a recently usurped dynasty plot to take back their homeland from across the Narrow Sea.')
-addBook(book);
-
-showBooks();
+library.showBooks();
